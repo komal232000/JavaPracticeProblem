8354366ac6ca59b96ebffb8d4f8f16a103e71dd6
Komal <khudekomal24@gmail.com>
2023-12-20 11:24:55 +0530

    Employee wage problem

---
+++ EmployeePresentAbsent.java
@@ -0,0 +1,10 @@
+package UC1_Employee_Wage;
+
+public class EmployeePresentAbsent {
+
+	public static void main(String[] args) {
+		System.out.println("Employee Wage computation problem!");
+
+	}
+
+}
